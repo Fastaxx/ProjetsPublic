a52c256b3424556cb92b86c5c0eb514cd1f39d95
Fastaxx <Fastaxx@github.com>
2022-01-10 17:51:18 +0100

    test

--- CRUDApp/app.py
+++ CRUDApp/app.py
@@ -1,17 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Mon Jan 10 17:13:41 2022
+Created on Mon Jan 10 17:46:45 2022
 
 @author: Louis
 """
 import streamlit as st
 import pandas as pd
-from gsheetsdb import connect
 
-st.write("My First Streamlit Web App")
+st.title("My First Streamlit Web App")
 
 df = pd.DataFrame({"one": [1, 2, 3], "two": [4, 5, 6], "three": [7, 8, 9]})
 st.write(df)
-
-
